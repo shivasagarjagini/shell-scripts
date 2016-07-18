8cf631278c579836d3ddea4fc3ae5a7e64f7cc46
Shiva Sagar <shivasagar9@github.com>
2016-07-18 16:53:52 -0400

    script to display fullname and username

--- script_ask.sh
+++ script_ask.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
+# This Script is written by Shiva Sagar (shivasagarjagini@github.com)
+# Script to print your full name and username.
+# How to RUN: "./script_ask.sh"
+
 echo "Enter your full name"
 read name
 echo "Your name is $name and your username is `echo $LOGNAME`"
