c9db21664a376807f5e76d0284e27e7ee080cd0e
Shiva Sagar <shivasagar9@github.com>
2016-07-18 17:33:03 -0400

    script to restart tomat

--- script_restart.sh
+++ script_restart.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
-#written by Shiva, contact *@.com if any issues
+
+# This Script is written by Shiva Sagar (shivasagarjagini@github.com)
+# Script to restart tomcat.
+# How to RUN: "./script_restart.sh"
+
 if [ `ls -lrt /home/ec2-user/tomcat/apache-tomcat-8.0.33/temp/ | wc -l` -lt 2 ]; then
 echo "creating an empty file - empty.txt"
 touch /home/ec2-user/tomcat/apache-tomcat-8.0.33/temp/empty.txt
