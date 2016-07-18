4c9efa20ce9d507ec4ca49e7c03f6c01aa37b03d
Shiva Sagar <shivasagar9@github.com>
2016-07-18 17:22:29 -0400

    commiting script_find.sh script_email.sh

--- script_email.sh
+++ script_email.sh
@@ -2,7 +2,7 @@
 
 # This Script is written by Shiva Sagar (shivasagarjagini@github.com)
 # Script to send  an email alert.
-# How to RUN: "./script_array.sh"
+# How to RUN: "./script_email.sh"
 
 if [ `find /home/ec2-user/tomcat/apache-tomcat-8.0.33/logs -type f | wc -l` -gt 4 ]; then
 #mail -s "Alert" shivasagarjagini@gmail.com
--- script_find.sh
+++ script_find.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
+# This Script is written by Shiva Sagar (shivasagarjagini@github.com)
+# Script to find a file.
+# How to RUN: "./script_find.sh"
+
 if [ -f /home/ec2-user/myname.txt -o `find /home/ec2-user/tomcat/apache-tomcat-8.0.33/logs -type f | wc -l` -gt 4 ]; then 
 echo "Files exist"
 fi
