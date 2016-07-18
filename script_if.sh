5136afdab6d47bf8dc8751fc49d9a844437d25c8
Shiva Sagar <shivasagar9@github.com>
2016-07-18 17:27:51 -0400

    script to use if

--- script_if.sh
+++ script_if.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
+# This Script is written by Shiva Sagar (shivasagarjagini@github.com)
+# Script to use if command.
+# How to RUN: "./script_if.sh"
+
 if [ shiva=kranthi ]; then
 echo "Strings are equal"
 fi
