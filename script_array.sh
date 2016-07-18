6100df7d76145a07c720b3181426ebbed2beac2c
Shiva Sagar <shivasagar9@github.com>
2016-07-18 16:49:09 -0400

    script to print a line using array

--- script_array.sh
+++ script_array.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/sh
 
+# This Script is written by Shiva Sagar (shivasagarjagini@github.com)
+# Script to print a line using array.
+# How to RUN: "./script_array.sh"
+
 for (( i=0; i<=10; i++ ))
 do 
-echo "This is printed $i time"
+echo "This is line $i"
 done
