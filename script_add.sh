b879239fa3e7904aed88e748f3f95916e19fd719
Shiva Sagar <shivasagar9@github.com>
2016-07-18 16:45:08 -0400

    script to add two integers

--- script_add.sh
+++ script_add.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/sh
 
+# This Script is written by Shiva Sagar (shivasagarjagini@github.com)
+# Script to perform addition of two integers
+# How to RUN: "./script_add.sh [first integer] [second integer]"
+
+if [ $# -ne 2 ]; then
+echo "command execution error"
+else
+echo $1 $2
+echo "The sum of $1 and $2 is `expr $1 + $2`"
+fi
+
+# How to RUN: "./script_add.sh"
+
 #echo "Please enter the first integer:"
 #read "first"
 #echo "Please enter the second integer:"
 #read "second"
 #echo "The sum of integers $first and $second are `expr $first + $second`."
-
-echo $1 $2
-echo "The sum of $1 and $2 are `expr $1 + $2`"
