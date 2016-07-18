731022740f7338cf0fe6c173ec8ca34242de9f0b
Shiva Sagar <shivasagar9@github.com>
2016-07-18 17:42:33 -0400

    script to switch two inputs

--- script_switch.sh
+++ script_switch.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
+# This Script is written by Shiva Sagar (shivasagarjagini@github.com)
+# Script to switch two inputs.
+# How to RUN: "./script_switch.sh"
+
 echo "Enter the two parameters A and B"
 read A B
 echo "The output is $B $A"
